28ee2deb4eacb034b8455dc17bdc046988630957
Nilo Freitas <nilo.freitas@agenciam3.com>
2023-09-20 18:45:18 -0300

    feat(home): implementando o banner tarja mobile

--- src/components/BannerLine/index.jsx
+++ src/components/BannerLine/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import Banner from '../../assets/banner-tarja.jpg'
+import BannerMobile from '../../assets/banner-tarja-mobile.jpg'
 
 import './styles.scss';
 
 export function BannerLine() {
   return (
     <div className="container-banner-tarja" id="tarja">
-      <a href="https://lojadabotina.com.br/">
+      <a href="https://lojadabotina.com.br/" className="banner-tarja-desktop">
         <img src={Banner} alt="Banner tarja" />
+      </a>
+      <a href="https://lojadabotina.com.br/" className="banner-tarja-mobile">
+        <img src={BannerMobile} alt="Banner tarja" />
       </a>
     </div>
   );
